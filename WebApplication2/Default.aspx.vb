b27f632b13a4a214d768c579ed993658760eb007
r78977 <taka-h@ryobi.co.jp>
2021-08-10 10:49:31 +0900

    first commit

---
+++ WebApplication2/Default.aspx.vb
@@ -0,0 +1,15 @@
+﻿Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        If IsPostBack Then
+        Else
+            Session("server") = Server.MachineName
+        End If
+
+        lbl.Text = Session("server")
+
+    End Sub
+
+End Class
